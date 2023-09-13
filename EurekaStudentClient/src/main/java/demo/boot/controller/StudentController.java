615f2c0766508ea8da7e4dd49e839e36266328cc
NamrathShetty <64630539+NamrathShetty@users.noreply.github.com>
2023-09-13 10:43:50 +0530

    Add files via upload

---
+++ EurekaStudentClient/src/main/java/demo/boot/controller/StudentController.java
@@ -0,0 +1,68 @@
+package demo.boot.controller;
+
+import java.util.List; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.MediaType;
+
+import demo.boot.model.Student;
+import demo.boot.service.StudentServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	@Autowired
+	RestTemplate restTemplate;
+
+	private final StudentServiceImpl studentServiceImpl;
+
+	@Autowired
+	public StudentController(StudentServiceImpl studentServiceImpl) {
+		this.studentServiceImpl = studentServiceImpl;
+	}
+
+	@GetMapping
+    @CircuitBreaker(name="StudentService",fallbackMethod="fallbackMethod")
+	public ResponseEntity<List<Student>> getAllStudents() {
+		return studentServiceImpl.getAllStudents();
+	}
+
+	@GetMapping(value="/{id}",produces= { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+    @CircuitBreaker(name="StudentService",fallbackMethod="fallbackMethod")
+	public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+		return studentServiceImpl.getStudentById(id);
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CircuitBreaker(name="StudentService",fallbackMethod="fallbackMethod")
+	public ResponseEntity<?> createStudent(@RequestBody Student student) {
+		return studentServiceImpl.createStudent(student);
+	}
+
+	@PutMapping(value="/{id}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CircuitBreaker(name="StudentService",fallbackMethod="fallbackMethod")
+	public ResponseEntity<?> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+		return studentServiceImpl.updateStudent(id, student);
+	}
+
+	@DeleteMapping("/{id}")
+    @CircuitBreaker(name="StudentService",fallbackMethod="fallbackMethod")
+	public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
+		return studentServiceImpl.deleteStudent(id);
+	}
+
+	public ResponseEntity<Void> fallbackMethod(Throwable t) {
+		return ResponseEntity.notFound().build();
+	}
+	
+}
